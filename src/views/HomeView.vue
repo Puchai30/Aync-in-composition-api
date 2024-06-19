5d7a171eb8acb4c1951161b55d9837294251a77b
zaw <zaw@zaws-MacBook-Pro.local>
2024-06-19 20:06:23 +0700

    fetching posts data with aync and await

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,14 @@
   <div class="home">
     <h1>Hello Composition Api</h1>
 
-    <PostsList :home_posts="posts"></PostsList>
+    <div v-if="error">
+      {{ error }}
+    </div>
+
+    <div v-if="posts.length > 0">
+      <PostsList :home_posts="posts"></PostsList>
+    </div>
+    <div v-else>loading.....</div>
   </div>
 </template>
 
@@ -14,6 +21,23 @@ export default {
   components: { PostsList },
   setup() {
     let posts = ref([]);
+    let error = ref("");
+
+    let load = async () => {
+      try {
+        let response = await fetch("http://localhost:3000/posts");
+        if (response.status === 404) {
+          throw new Error("Url Not Found");
+        }
+        let datas = await response.json();
+        posts.value = datas;
+      } catch (err) {
+        error.value = err;
+        // console.log(err.message);
+      }
+    };
+    load();
+    return { posts, error };
   },
 };
 </script>
