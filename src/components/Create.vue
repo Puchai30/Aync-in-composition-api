3ba8575675640611ab50c90fde1651cb83f39957
zaw <zaw@zaws-MacBook-Pro.local>
2024-06-20 12:31:11 +0700

    create post backend

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,5 +1,5 @@
 <template>
-  <form action="">
+  <form @submit.prevent="addPost">
     <label>Title</label>
     <input required type="text" v-model="title" />
 
@@ -7,13 +7,15 @@
     <textarea required v-model="body"></textarea>
 
     <label>Tags(hit enter to add a tags)</label>
-    {{ tags }}
     <input
       required
       type="text"
       v-model="tag"
       @keydown.enter.prevent="handleKeyDown"
     />
+    <div v-for="tag in tags" :key="tag" class="pill">
+      {{ tag }}
+    </div>
 
     <button>Add Post</button>
   </form>
@@ -36,7 +38,21 @@ export default {
       tag.value = "";
     };
 
-    return { title, body, tag, tags, handleKeyDown };
+    let addPost = async () => {
+      await fetch("http://localhost:3000/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "applciation/json",
+        },
+        body: JSON.stringify({
+          title: title.value,
+          body: body.value,
+          tags: tags.value,
+        }),
+      });
+    };
+
+    return { title, body, tag, tags, handleKeyDown, addPost };
   },
 };
 </script>
--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,8 +1,14 @@
 <template>
-  <router-link :to="{ name: 'detail', params: { id: posts_list.id } }">
-    <h3>{{ posts_list.title }}</h3>
-  </router-link>
-  <p>{{ cutPostBody }}</p>
+  <div class="post">
+    <router-link :to="{ name: 'detail', params: { id: posts_list.id } }">
+      <h3>{{ posts_list.title }}</h3>
+    </router-link>
+    <p>{{ cutPostBody }}</p>
+
+    <div v-for="tag in posts_list.tags" :key="tag" class="pill">
+      {{ tag }}
+    </div>
+  </div>
 </template>
 
 <script>
@@ -19,4 +25,39 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.post {
+  margin: 0 40px 30px;
+  padding-bottom: 30px;
+  border-bottom: 1px dashed #e7e7e7;
+}
+.post h3 {
+  display: inline-block;
+  position: relative;
+  font-size: 26px;
+  color: white;
+  margin-bottom: 10px;
+  max-width: 400px;
+}
+.post h3::before {
+  content: "";
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: #ff8800;
+  position: absolute;
+  z-index: -1;
+  padding-right: 40px;
+  left: -30px;
+  transform: rotateZ(-1deg);
+}
+.pill {
+  display: inline-block;
+  margin: 10px 10px 0 0;
+  color: #444;
+  background: #ddd;
+  padding: 8px;
+  border-radius: 20px;
+  font-size: 14px;
+}
+</style>
